841e90ed5cea807735adbf478da2cdb2eebf8a37
Saideep <kotasa01@pfw.edu>
2025-09-04 16:28:48 -0400

    basic face detect

--- OpenCV/face_detect.py
+++ OpenCV/face_detect.py
@@ -1,17 +1,31 @@
 import cv2 as cv
 
+def rescaleFrame(frame, scale=0.6): #60% of original size
+    #works for images, videos and live video
+    width = int(frame.shape[1] * scale) #shape[1] is the width
+    height = int(frame.shape[0] * scale) #shape[0] is the height
+    dimensions = (width, height)
+
+    return cv.resize(frame, dimensions, interpolation=cv.INTER_AREA) #INTER_AREA is used for shrinking
+
 img = cv.imread('Photos/man.png')
-cv.imshow('Image', img)
+#cv.imshow('Image', img)
+resized = rescaleFrame(img)
+cv.imshow('Resized Image', resized)
 
-gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
-cv.imshow('Gray Image', gray)
+gray = cv.cvtColor(resized, cv.COLOR_BGR2GRAY)
+#cv.imshow('Gray Image', gray)
+resized_gray = rescaleFrame(gray)
+cv.imshow('Resized Gray Image', resized_gray)
 
-haar_cascade = cv.CascadeClassifier('haar_face.xml') # Load Haar Cascade
+haar_cascade = cv.CascadeClassifier('OpenCV/haar_face.xml') # Load Haar Cascade
 
-faces_rect = haar_cascade.detectMultiScale(gray, scaleFactor=1.1, minNeighbors=3) # Detect faces
+faces_rect = haar_cascade.detectMultiScale(resized_gray, scaleFactor=1.1, minNeighbors=4) # Detect faces
+
+print(f'Number of faces found: {len(faces_rect)}')
 
 for (x, y, w, h) in faces_rect:
-    cv.rectangle(img, (x, y), (x+w, y+h), (0, 255, 0), 2)
+    cv.rectangle(resized_gray, (x, y), (x+w, y+h), (0, 255, 0), 2)
 
-cv.imshow('Detected Faces', img)
+cv.imshow('Detected Faces', resized)
 cv.waitKey(0)
--- OpenCV/rescale.py
+++ OpenCV/rescale.py
@@ -15,10 +15,10 @@ def rescaleFrame(frame, scale=0.75): #75% of original size
     return cv.resize(frame, dimensions, interpolation=cv.INTER_AREA) #INTER_AREA is used for shrinking
 
 
-def changeRes(width, height):
-    #works only for live video
-    capture.set(3, width) #3 is the width
-    capture.set(4, height) #4 is the height
+# def changeRes(width, height):
+#     #works only for live video
+#     capture.set(3, width) #3 is the width
+#     capture.set(4, height) #4 is the height
 
 resized_image = rescaleFrame(img)
 cv.imshow('Original', img)
