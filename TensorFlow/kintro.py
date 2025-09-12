dd47c786577fa239d7aab60499303123fe7a2714
Saideep <kotasa01@pfw.edu>
2025-09-12 16:16:36 -0400

    got kintro from microsoft course

--- TensorFlow/kintro.py
+++ TensorFlow/kintro.py
@@ -1,43 +1,67 @@
-import os # For file path operations
-os.environ['TF_CPP_MIN_LOG_LEVEL'] = '-1' # Suppress TensorFlow logging
-import tensorflow as tf
-import numpy as np
 import gzip
+import numpy as np
+import tensorflow as tf
 from typing import Tuple
 
-class NeuralNetwork(tf.keras.Model): # Define a neural network model by subclassing tf.keras.Model
-  def __init__(self):
-    super(NeuralNetwork, self).__init__()
-    self.sequence = tf.keras.Sequential([ # Define the layers of the model using tf.keras.Sequential
-      tf.keras.layers.Flatten(input_shape=(28, 28)), # Flatten the input images from 28x28 to a 784-dimensional vector
-      tf.keras.layers.Dense(128, activation='relu'), # First dense layer with 128 units and ReLU activation
-      tf.keras.layers.Dense(20, activation='relu'), # Second dense layer with 20 units and ReLU activation
-      tf.keras.layers.Dense(10) # Output layer with 10 units (one for each class)
-    ])
+def read_images(path: str, image_size: int, num_items: int) -> np.ndarray:
+  with gzip.open(path, 'rb') as file:
+    data = np.frombuffer(file.read(), np.uint8, offset=16)
+    data = data.reshape(num_items, image_size, image_size)
+  return data
 
-  def call(self, x: tf.Tensor) -> tf.Tensor: # Define the forward pass of the model
-    # input x is a batch of images with shape (batch_size, 28, 28)
-    y_prime = self.sequence(x) # Pass the input through the layers
-    return y_prime # Return the output logits
-  
-def get_data() -> Tuple[tf.data.Dataset, tf.data.Dataset]:
-  # Load the Fashion MNIST dataset from TensorFlow Keras datasets
-  (training_images, training_labels), (test_images, test_labels) = tf.keras.datasets.fashion_mnist.load_data()
+def read_labels(path: str, num_items: int) -> np.ndarray:
+  with gzip.open(path, 'rb') as file:
+    data = np.frombuffer(file.read(num_items + 8), np.uint8, offset=8)
+    data = data.astype(np.int64)
+  return data
 
-  # Create TensorFlow datasets from the training and test images and labels
+def get_data(batch_size: int) -> Tuple[tf.data.Dataset, tf.data.Dataset]:
+  image_size = 28
+  num_train = 60000
+  num_test = 10000
+
+  training_images = read_images('data/FashionMNIST/raw/train-images-idx3-ubyte.gz', image_size, num_train)
+  test_images = read_images('data/FashionMNIST/raw/t10k-images-idx3-ubyte.gz', image_size, num_test)
+  training_labels = read_labels('data/FashionMNIST/raw/train-labels-idx1-ubyte.gz', num_train)
+  test_labels = read_labels('data/FashionMNIST/raw/t10k-labels-idx1-ubyte.gz', num_test)
+
+  # (training_images, training_labels), (test_images, test_labels) = tf.keras.datasets.fashion_mnist.load_data()
+
   train_dataset = tf.data.Dataset.from_tensor_slices((training_images, training_labels))
   test_dataset = tf.data.Dataset.from_tensor_slices((test_images, test_labels))
 
-  # Normalize the images to [0, 1] range by dividing by 255.0
   train_dataset = train_dataset.map(lambda image, label: (float(image) / 255.0, label))
   test_dataset = test_dataset.map(lambda image, label: (float(image) / 255.0, label))
 
-  # Define batch size
-  batch_size = 64 
-
-  # Shuffle and batch the datasets
   train_dataset = train_dataset.batch(batch_size).shuffle(500)
   test_dataset = test_dataset.batch(batch_size).shuffle(500)
 
-  return train_dataset, test_dataset
+  return (train_dataset, test_dataset)
 
+
+class NeuralNetwork(tf.keras.Model):
+  def __init__(self):
+    super(NeuralNetwork, self).__init__()
+    self.sequence = tf.keras.Sequential([
+      tf.keras.layers.Flatten(input_shape=(28, 28)),
+      tf.keras.layers.Dense(20, activation='relu'),
+      tf.keras.layers.Dense(10)
+    ])
+
+  def call(self, x: tf.Tensor) -> tf.Tensor:
+    y_prime = self.sequence(x)
+    return y_prime
+
+  
+labels_map = {
+  0: 'T-Shirt',
+  1: 'Trouser',
+  2: 'Pullover',
+  3: 'Dress',
+  4: 'Coat',
+  5: 'Sandal',
+  6: 'Shirt',
+  7: 'Sneaker',
+  8: 'Bag',
+  9: 'Ankle Boot',
+}
